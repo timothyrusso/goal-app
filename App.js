34f5063d5b9ad6615d90241016603f22dc0cb908
timothyrusso <russotimothy@live.it>
2023-12-26 19:27:50 +0100

    feat: style the home page

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course gol!" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course gol!" style={styles.TextInput} />
         <Button title="Add a goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
       </View>
     </View>
@@ -17,6 +17,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC',
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: '#CCC',
+    padding: 10,
+    borderRadius: 10,
+    width: '70%',
+    marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
